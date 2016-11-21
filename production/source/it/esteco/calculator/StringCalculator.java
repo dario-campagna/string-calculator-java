dc2e8ba8def7b492764172b3ebe6f52c7197bdf7
dcampagna <campagna@esteco.com>
2016-11-21 17:40:49 +0100

    We now ignore numbers greater than 1000.

--- production/source/it/esteco/calculator/StringCalculator.java
+++ production/source/it/esteco/calculator/StringCalculator.java
@@ -12,12 +12,20 @@ public class StringCalculator {
     }
 
     public int add(String numbersAsText) throws Exception {
-        List<Integer> integers = parser.parse(numbersAsText);
-        List<Integer> negatives = integers.stream().filter(integer -> integer < 0).collect(Collectors.toList());
+        List<Integer> integers = getNumbersGreaterThanOneThousand(numbersAsText);
+        List<Integer> negatives = getNegativeIntegers(integers);
         if (negatives.isEmpty()) {
             return integers.stream().reduce(0, (a, b) -> a + b);
         } else {
             throw new Exception("Negatives not allowed " + negatives);
         }
+    }
+
+    private List<Integer> getNumbersGreaterThanOneThousand(String numbersAsText) {
+        return parser.parse(numbersAsText).stream().filter(integer -> integer < 1001).collect(Collectors.toList());
+    }
+
+    private List<Integer> getNegativeIntegers(List<Integer> integers) {
+        return integers.stream().filter(integer -> integer < 0).collect(Collectors.toList());
     }
 }
