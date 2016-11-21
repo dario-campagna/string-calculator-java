01ae2cd5ff595e465a1b7a8f3b3d64262a4df678
dcampagna <campagna@esteco.com>
2016-11-21 16:35:09 +0100

    We now throw an exception on negative numbers.

--- production/source/it/esteco/calculator/StringCalculator.java
+++ production/source/it/esteco/calculator/StringCalculator.java
@@ -1,5 +1,8 @@
 package it.esteco.calculator;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringCalculator {
 
     private NumbersParser parser;
@@ -8,7 +11,13 @@ public class StringCalculator {
         this.parser = parser;
     }
 
-    public int add(String numbersAsText) {
-        return parser.parse(numbersAsText).stream().reduce(0, (a, b) -> a + b);
+    public int add(String numbersAsText) throws Exception {
+        List<Integer> integers = parser.parse(numbersAsText);
+        List<Integer> negatives = integers.stream().filter(integer -> integer < 0).collect(Collectors.toList());
+        if (negatives.isEmpty()) {
+            return integers.stream().reduce(0, (a, b) -> a + b);
+        } else {
+            throw new Exception("Negatives not allowed " + negatives);
+        }
     }
 }
---
+++ test/source/it/esteco/calculator/ThrowExceptionOnNegativeNumbers.java
@@ -0,0 +1,45 @@
+package it.esteco.calculator;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ThrowExceptionOnNegativeNumbers {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    private String message;
+    private String numbers;
+
+    public ThrowExceptionOnNegativeNumbers(String message, String numbers) {
+        this.message = message;
+        this.numbers = numbers;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<String[]> data() {
+        return Arrays.asList(new String[][]{
+                {"Negatives not allowed [-6]", "-6"},
+                {"Negatives not allowed [-5]", "2,-5"},
+                {"Negatives not allowed [-12, -11, -1]", "//:\n-12:-11:-1"}
+        });
+    }
+
+    @Test
+    public void negativesNotAllowed() throws Exception {
+        expectedException.expect(Exception.class);
+        expectedException.expectMessage(message);
+
+        new StringCalculator(new NumbersParser()).add(numbers);
+    }
+}
+
