48f4cc4ddc69f7a6c55290b6a1d8f5c6e287bac8
dcampagna <campagna@esteco.com>
2016-11-21 13:43:53 +0100

    Removed obsolete code.

--- production/source/it/esteco/calculator/StringCalculator.java
+++ production/source/it/esteco/calculator/StringCalculator.java
@@ -4,15 +4,13 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
-        } else if (numbers.length() > 1) {
+        } else {
             String[] numbersAsStrings = numbers.split(",");
             int sum = 0;
             for (String number : numbersAsStrings) {
                 sum += Integer.valueOf(number);
             }
             return sum;
-        } else {
-            return Integer.valueOf(numbers);
         }
     }
 }
