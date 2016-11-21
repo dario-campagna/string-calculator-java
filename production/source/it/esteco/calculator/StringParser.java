c1cf2f28d56d7069ef8cf5acc7e8d08e02d9cdf5
dcampagna <campagna@esteco.com>
2016-11-21 15:00:47 +0100

    Removed obsolete code.

--- production/source/it/esteco/calculator/StringParser.java
+++ production/source/it/esteco/calculator/StringParser.java
@@ -1,25 +1,26 @@
 package it.esteco.calculator;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringParser {
 
     public List<Integer> parse(String numbers) {
-        if (numbers.startsWith("//")) {
-            char delimiter = numbers.charAt(2);
-            String substring = numbers.substring(4, numbers.length());
-            return Arrays.stream(substring.split("" + delimiter))
-                    .map(stringToInteger())
-                    .collect(Collectors.toList());
-        } else {
-            return Arrays.stream(numbers.split("[\n,]"))
-                    .map(stringToInteger())
-                    .collect(Collectors.toList());
+        String delimiter = "[\n,]";
+        Pattern pattern = Pattern.compile("//([^\\d]*?)\n");
+        Matcher matcher = pattern.matcher(numbers);
+        if (matcher.find()) {
+            delimiter = matcher.group(1).toString();
+            numbers = numbers.substring(4, numbers.length());
+
         }
+        return Arrays.stream(numbers.split(delimiter))
+                .map(stringToInteger())
+                .collect(Collectors.toList());
     }
 
     private Function<String, Integer> stringToInteger() {
