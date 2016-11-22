d3b7197db17608088c8e5477240525e15c8ce313
dcampagna <campagna@esteco.com>
2016-11-22 10:41:16 +0100

    Removed obsolete code.

--- production/source/it/esteco/calculator/Calculator.java
+++ production/source/it/esteco/calculator/Calculator.java
@@ -4,10 +4,6 @@ public class Calculator {
     public static int add(String string) {
         if (string.isEmpty()) {
             return 0;
-        } else if (string.length() == 1){
-            return Integer.valueOf(string);
-        } else if (string.length() == 3) {
-            return Integer.valueOf(string.substring(0,1)) + Integer.valueOf(string.substring(2,3));
         } else {
             String[] tokens = string.split(",");
             int sum = 0;
