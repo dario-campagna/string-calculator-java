2a604a7c3aed68ed008ba08640ccd6337e9aab95
dcampagna <campagna@esteco.com>
2016-11-22 10:53:40 +0100

    Removed obsolete code.

--- production/source/it/esteco/calculator/Calculator.java
+++ production/source/it/esteco/calculator/Calculator.java
@@ -1,16 +1,20 @@
 package it.esteco.calculator;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Calculator {
     public static int add(String string) {
-        if (string.isEmpty()) {
-            return 0;
-        } else {
-            String[] tokens = string.split(",|\n");
-            int sum = 0;
-            for (String token : tokens) {
-                sum += Integer.valueOf(token);
-            }
-            return sum;
-        }
+        String[] tokens = tokenize(string);
+        IntStream intStream = convertToIntegers(tokens);
+        return intStream.reduce(0, (a, b) -> a + b);
+    }
+
+    private static String[] tokenize(String string) {
+        return string.isEmpty() ? new String[0] : string.split(",|\n");
+    }
+
+    private static IntStream convertToIntegers(String[] tokens) {
+        return Arrays.stream(tokens).mapToInt(Integer::parseInt);
     }
 }
